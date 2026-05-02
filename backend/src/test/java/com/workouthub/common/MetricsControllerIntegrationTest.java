23ed67ffc7d3a85a30f521ab1a37f5108dc21c6e
aciro <aciroglu.fatih@gmail.com>
2026-05-02 21:23:38 +0300

    test(04-01): add MetricsController integration test

---
+++ backend/src/test/java/com/workouthub/common/MetricsControllerIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.workouthub.common;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.workouthub.support.AbstractIntegrationTest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+class MetricsControllerIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired MockMvc mvc;
+
+    @Test
+    void metricsReturns200WithPrometheusTextFormatWithoutAuth() throws Exception {
+        mvc.perform(get("/metrics"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("text/plain"))
+                .andExpect(content().string(Matchers.containsString("# HELP")));
+    }
+
+    @Test
+    void actuatorPrometheusStillReachableForBackwardCompat() throws Exception {
+        mvc.perform(get("/actuator/prometheus"))
+                .andExpect(status().isOk());
+    }
+}
